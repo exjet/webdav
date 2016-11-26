4d35aa5d85bd0bd22622138ef7c04a094a3b1387
Геннадий Музыкантов <muzykantov@mac.local>
2016-11-26 13:05:03 +0300

    Add digest authentication for windows clients

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-htpasswd -cb /etc/apache2/webdav.password $USERNAME $PASSWORD
+rm -f /etc/apache2/webdav.password
+(echo -n "$USERNAME:webdav:" && echo -n "$USERNAME:webdav:$PASSWORD" | md5sum | cut -d' ' -f1) > /etc/apache2/webdav.password
 chown root:www-data /etc/apache2/webdav.password
 chmod 640 /etc/apache2/webdav.password
 
